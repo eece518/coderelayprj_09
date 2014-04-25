02c66bd1292f864ecee149018da6c957687f9cf6
haijunq <haijunq@gmail.com>
2014-04-24 22:42:42 -0700

    init

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<body>
+<p>HTML5 CANVAS</p>
+<canvas id="Canvas" width="400" height="300" style="border:2px solid #000000;">
+Your browser does not support the HTML5 canvas tag.
+</canvas>
+
+</body>
+</html>
